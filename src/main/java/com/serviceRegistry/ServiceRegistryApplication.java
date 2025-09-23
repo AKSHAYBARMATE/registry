d3fbace893dc9bc34d0b6a7c44f1158b5786eaf7
AKSHAYBARMATE <100841949+AKSHAYBARMATE@users.noreply.github.com>
2025-09-23 15:02:12 +0530

    Update ServiceRegistryApplication.java

--- src/main/java/com/serviceRegistry/ServiceRegistryApplication.java
+++ src/main/java/com/serviceRegistry/ServiceRegistryApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@SpringBootApplication(exclude = SystemMetricsAutoConfiguration.class)
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
